6d48b2026198f15fcd4ccdee8b6489f1557a5bb3
jongseok Kwon <86949394+himJJong@users.noreply.github.com>
2022-02-18 22:49:51 +0900

    Create ArrayTest2.java

---
+++ array/ArrayTest2.java
@@ -0,0 +1,14 @@
+package array;
+
+public class ArrayTest2 {
+
+	public static void main(String[] args) {
+		int[] data = new int[] {1,2,3,4,5};
+		
+		for(int i=0; i<data.length;i++) {
+			System.out.println(data[i]);
+		}
+		
+	}
+
+}

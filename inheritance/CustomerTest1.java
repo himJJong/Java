f5827fc3c502c804c6e8033b610e2211838f17f6
jongseok Kwon <86949394+himJJong@users.noreply.github.com>
2022-02-21 03:04:07 +0900

    Update CustomerTest1.java

--- inheritance/CustomerTest1.java
+++ inheritance/CustomerTest1.java
@@ -3,15 +3,11 @@ package inheritance;
 public class CustomerTest1 {
 
 	public static void main(String[] args) {
-		Customer customerLee = new Customer();
-		customerLee.setCustomerID(10010);
-		customerLee.setCustomerName("이순신" );
+		Customer customerLee = new Customer(100,"서민 ");
 		customerLee.bonusPoint = 1000;
 		System.out.println(customerLee.showCustomerInfo());
 		
-		VIPCustomer customerKim = new VIPCustomer();
-		customerKim.setCustomerID(10020);
-		customerKim.setCustomerName("김유" );
+		VIPCustomer customerKim = new VIPCustomer(100000,"부자",20);
 		customerKim.bonusPoint = 10000;
 		System.out.println(customerKim.showCustomerInfo());
 

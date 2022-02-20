09448793c04b6e1d432399e82ff9bf39e04371f2
jongseok Kwon <86949394+himJJong@users.noreply.github.com>
2022-02-21 03:05:32 +0900

    Update VIPCustomer.java

--- inheritance/VIPCustomer.java
+++ inheritance/VIPCustomer.java
@@ -4,11 +4,23 @@ public class VIPCustomer extends Customer {
 	private int agentID;
 	double saleRatio;
 	
-	public VIPCustomer() {
+	
+	
+	public VIPCustomer(int customerID, String customerName, int agentID) {
+		super(customerID,customerName);
 		customerGrade = "VIP";
 		bonusRatio = 0.05;
 		saleRatio = 0.1;
+		this.agentID=agentID;
+		System.out.println("VIPCustomer(int,String,int) 생성자 호출");
 	}
+	
+	@Override
+	public int calcPrice(int price) {
+		bonusPoint += price * bonusRatio;
+		return price - (int)(price * saleRatio);
+	}
+
 	public int getAgentID() {
 		return agentID;
 	}

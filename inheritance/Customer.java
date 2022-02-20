abe648c7d989e39a2f21461010c5c169cd34d388
jongseok Kwon <86949394+himJJong@users.noreply.github.com>
2022-02-21 03:03:51 +0900

    Update Customer.java

--- inheritance/Customer.java
+++ inheritance/Customer.java
@@ -8,12 +8,15 @@ public class Customer {
 	int bonusPoint;
 	double bonusRatio;
 	
-	public Customer() {
+	public Customer(int customerID, String customerName) {
+		this.customerID = customerID;
+		this.customerName = customerName;
 		customerGrade="SILVER";
 		bonusRatio=0.01;
+		System.out.println("Customer(int,String) 생성자 호출");
 	}
 	
-	public int calPrice(int price) {
+	public int calcPrice(int price) {
 		bonusPoint += price*bonusRatio;
 		return price;
 	}
@@ -31,7 +34,7 @@ public class Customer {
 		this.customerID=customerID;
 	}
 	
-	public String getCusomerName() {
+	public String getCustomerName() {
 		return customerName;
 	}
 	
